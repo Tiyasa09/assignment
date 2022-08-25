bcc6475c241339f73721f9ece0d205598ec28abe
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-25 17:15:36 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,19 @@
+package operator1;
+import java.util.Scanner;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year=0;
+		System.out.println("enter a year:");
+		Scanner sc=new Scanner(System.in);
+		year=sc.nextInt();
+		while(year/4==0);
+		System.out.println("This is a leapyear");
+	}}
+		
+
+	
+
+
