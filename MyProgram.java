c0a4be20e196cb44174fee224278dc59bd2d7123
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-09-01 00:13:13 +0530

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,9 @@
+package object;
+
+public class MyProgram {
+
+	public static void main(String args[]) {
+		System.out.println("My First Java Program");
+	}}
+
+
---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+package com.cognizant.shapes;
+
+ class AreaCalculator {
+	void calculateArea() {
+		System.out.println("The area of a rectangle is calculated using the formula length*breadth");
+		}}
+	class Rectangle{
+		public static void main(String[] args) {
+			AreaCalculator a=new AreaCalculator();
+	a.calculateArea();
+		}}
+
+
