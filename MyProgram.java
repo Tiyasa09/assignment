c345902dd164569f9cdaf97dba84991c53145892
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-09-01 11:50:56 +0530

    Add files via upload

--- MyProgram.java
+++ MyProgram.java
@@ -1,9 +1,11 @@
-package object;
+package operator1;
 
 public class MyProgram {
 
-	public static void main(String args[]) {
-		System.out.println("My First Java Program");
-	}}
+	public static void main(String[] args) {
+		DisplayMessage program=new DisplayMessage();
+		program.printMessage();
 
+	}
 
+}
