496e6f6cbac7d73d3d3a82ef0533055be0698438
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-10-18 17:56:05 +0530

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,18 +1,18 @@
 package exception;
 import java.util.InputMismatchException;
-import java.util.Scanner;
-public class Assignment {
+import java.util.Scanner;//import scanner class
+public class Assignment { 
 	public static void main(String[] args) {
-		try {
+		try { //try block
 		Scanner sc=new Scanner(System.in);
-		int x=sc.nextInt();
-		int y=sc.nextInt();
+		int x=sc.nextInt();//1st integer
+		int y=sc.nextInt();//2nd integer
 		System.out.println("resust" +(x/y));
-	}catch(InputMismatchException e) {
+	}catch(InputMismatchException e) {//catch block
 		System.out.println(e);
-	}catch(ArithmeticException f) {
+	}catch(ArithmeticException f) { //2nd catch block
 		System.out.println(f);
-	}catch(Exception a) {
+	}catch(Exception a) { 
 		System.out.println(a);
 	}
 	
